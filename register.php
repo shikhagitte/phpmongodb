d68c44e36343741ef1dcbea03cd86a540ed933aa
shikhagitte <57101929+shikhagitte@users.noreply.github.com>
2019-12-01 19:17:52 +0530

    Update register.php

--- register.php
+++ register.php
@@ -2,20 +2,18 @@
 /*This is simple Register page*/
 session_start();
 require_once('connection.php');
-/*if(isset($_SESSION['user']))
-{
-    header('Location:home.php');
-}*/
 
 if (isset($_POST['username']) && isset($_POST['password']))
 {
     $username = $_POST['username'];
     $password = hash('sha256', $_POST['password']);
+    $email = $_POST['email'];
     $result = $db->users->insertOne(array('username'=>$username, 'password'=>$password));
     $result = $db->users->createIndex(array('username'=>1) , array('unique'=>true));
     header('Location:index.php');
 }
 ?>
+<!-- Sample code before adding CSS
 <html>
 <head>
     <title> REGISTER PAGE </title>
@@ -30,5 +28,83 @@ if (isset($_POST['username']) && isset($_POST['password']))
 
     </form>
     <a href="index.php">Already have an Account? login here.</a>
+</body>
+</html>!-->
+<!--After adding CSS!-->
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+.input-container {
+  display: -ms-flexbox; /* IE10 */
+  display: flex;
+  width: 100%;
+  margin-bottom: 15px;
+}
+
+.icon {
+  padding: 10px;
+  background:#778899;
+  color: white;
+  min-width: 50px;
+  text-align: center;
+}
+
+.input-field {
+  width: 100%;
+  padding: 10px;
+  outline: none;
+}
+
+.input-field:focus {
+  border: 2px solid #778899;
+}
+
+/* Set a style for the submit button */
+.btn {
+  background-color: #778899;
+  color: white;
+  padding: 15px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+.btn:hover {
+  opacity: 1;
+}
+</style>
+</head>
+<body>
+
+<form method ="post" action="regsiter.php" style="max-width:500px;margin:auto">
+  <h2>Register Form</h2>
+  <div class="input-container">
+    <i class="fa fa-user icon"></i>
+    <input class="input-field" type="text" placeholder="Username" name="username">
+  </div>
+
+  <div class="input-container">
+    <i class="fa fa-envelope icon"></i>
+    <input class="input-field" type="text" placeholder="Email" name="email">
+  </div>
+  
+  <div class="input-container">
+    <i class="fa fa-key icon"></i>
+    <input class="input-field" type="password" placeholder="Password" name="password">
+  </div>
+
+  <button type="submit" class="btn">Sign Up</button><br>
+  <a href="index.php">Already have an Account? login here.</a>
+
+</form>
+
 </body>
 </html>
