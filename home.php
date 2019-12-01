d885193a9b3b9c77417380c10ca9300ca3332508
shikhagitte <57101929+shikhagitte@users.noreply.github.com>
2019-12-01 19:20:53 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,3 +1,18 @@
 <?php
-echo " welcome";
+/*this is a homepage*/
+session_start();
+require_once('connection.php');
+if(!isset($_SESSION['user']))
+{
+    header("Location: index.php");
+}
+$data = $db->users->findOne(array('_id'=> $_SESSION['user']));
 ?>
+<html>
+<head>
+    <title>Sample</title>
+</head>
+<body>
+    <?php include('header.php');?>
+</body>
+</html>
